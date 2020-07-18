8f95a8fb5e96378dd7553c5c9b66df412355d5a4
Neil Botelho <neil.botelho321@gmail.com>
2020-07-18 22:41:12 +0530

    Add styling for projects index page

--- components/PostList.js
+++ components/PostList.js
@@ -4,20 +4,61 @@ export default function PostList({ posts }) {
   if (posts === 'undefined') return null
 
   return (
-    <div>
+    <div className="post-list">
       {!posts && <div>No posts!</div>}
-      <ul>
         {posts &&
           posts.map((post) => {
             return (
-              <li key={post.slug}>
-                <Link href={{ pathname: `/projects/${post.slug}` }}>
-                  <a>{post.frontmatter.title}</a>
-                </Link>
-              </li>
+              <Link href={{ pathname: `/projects/${post.slug}` }}>
+                <div key={post.slug} className="post-container">
+                  <p className="post-date">{post.frontmatter.date}</p>
+                  <br/>
+                  <p className="post-title">{post.frontmatter.title}</p>
+                  <p className="post-description">{post.frontmatter.description || "" }</p>
+                </div>
+              </Link>
             )
           })}
-      </ul>
+    <style jsx>{`
+      .post-list{
+        list-style-type: none;
+        text-align:left;
+        margin:6vh 6vh 6vh 6vh;
+        font-family: 'Overpass Mono', monospace;
+        display:flex;
+        flex-direction:column;
+        align-items:center;
+        // justify-content:center;
+      }
+      .post-container{
+        width:70%;
+        font-size:1rem;
+        box-shadow:1px 2px 3px 1px;
+        margin:2vh;
+        // margin:
+       transition: scale .2s ease-in-out;
+      }
+      .post-container:hover, .post-container:active{
+        transform:scale(1.01);
+        // border:2px solid;
+      }
+      .post-date{
+        margin:2vh 2vh 0vh 2vh ;
+        padding-bottom:0;
+        color:#7e7f7e;
+      }
+      .post-title{
+        font-weight:bold;
+        margin:0vh 2vh 2vh 2vh ;
+        padding-top:0;
+
+      }
+      .post-description{
+         margin:0vh 2vh 2vh 2vh ;
+        padding-top:0;
+        color:#323332;
+      }
+      `}</style>
     </div>
   )
 }
